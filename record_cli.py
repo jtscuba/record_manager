0478c95673a38055120ce0827c202d10b11e1762
Josh Trate <jtscuba@gmail.com>
2019-06-22 19:02:11 -0700

    server and tests

--- record_cli.py
+++ record_cli.py
@@ -5,15 +5,19 @@ from record_lib import RecordSortOrder, update_records, records_sorted_by_order
 
 
 def main(
-        comma_separated_file_name: str,
-        pipe_separated_file_name: str,
-        space_separated_file_name: str,
-        sort_order: RecordSortOrder
+    comma_separated_file_name: str,
+    pipe_separated_file_name: str,
+    space_separated_file_name: str,
+    sort_order: RecordSortOrder,
 ) -> None:
     records = []
     record_files = zip(
-        (comma_separated_file_name, pipe_separated_file_name, space_separated_file_name),
-        (',', '|', ' ')
+        (
+            comma_separated_file_name,
+            pipe_separated_file_name,
+            space_separated_file_name,
+        ),
+        (",", "|", " "),
     )
     for file_name, delimiter in record_files:
         with open(file_name) as file:
@@ -24,22 +28,36 @@ def main(
     print(
         tabulate.tabulate(
             sorted_records,
-            headers=('Last Name', 'First Name', 'Gender', 'Favorite Color', 'Date of Birth')
+            headers=(
+                "Last Name",
+                "First Name",
+                "Gender",
+                "Favorite Color",
+                "Date of Birth",
+            ),
         )
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sort records.")
-    parser.add_argument('comma_separated_records_file', help="file containing ',' separated records")
-    parser.add_argument('pipe_separated_records_file', help="file containing '|' separated records")
-    parser.add_argument('space_separated_records_file', help="file containing ' ' separated records")
-    parser.add_argument('output_sort_order', type=RecordSortOrder, choices=list(RecordSortOrder))
+    parser.add_argument(
+        "comma_separated_records_file", help="file containing ',' separated records"
+    )
+    parser.add_argument(
+        "pipe_separated_records_file", help="file containing '|' separated records"
+    )
+    parser.add_argument(
+        "space_separated_records_file", help="file containing ' ' separated records"
+    )
+    parser.add_argument(
+        "output_sort_order", type=RecordSortOrder, choices=list(RecordSortOrder)
+    )
     args = parser.parse_args()
 
     main(
         args.comma_separated_records_file,
         args.pipe_separated_records_file,
         args.space_separated_records_file,
-        args.output_sort_order
+        args.output_sort_order,
     )
--- record_lib.py
+++ record_lib.py
@@ -9,8 +9,8 @@ from typing import NamedTuple, List
 @unique
 @functools.total_ordering
 class Gender(Enum):
-    FEMALE = 'Female'
-    MALE = 'Male'
+    FEMALE = "Female"
+    MALE = "Male"
 
     def __str__(self):
         return self.value
@@ -26,43 +26,72 @@ class Record(NamedTuple):
     favorite_color: str
     date_of_birth: date
 
+    def to_dict(self):
+        return {
+            "last_name": self.last_name,
+            "first_name": self.first_name,
+            "gender": self.gender.value,
+            "favorite_color": self.favorite_color,
+            "date_of_birth": self.date_of_birth.strftime("%m/%d/%Y"),
+        }
+
 
 @unique
 class RecordSortOrder(Enum):
-    GENDER_AND_LAST_NAME_ASCENDING = 'gender_and_last_name_ascending'
-    DATE_OF_BIRTH_ASCENDING = 'date_of_birth_ascending'
-    LAST_NAME_DESCENDING = 'last_name_descending'
+    GENDER_AND_LAST_NAME_ASCENDING = "gender_and_last_name_ascending"
+    DATE_OF_BIRTH_ASCENDING = "date_of_birth_ascending"
+    LAST_NAME_DESCENDING = "last_name_descending"
 
     def __str__(self):
         return self.value
 
 
-def update_records(current_records: List[Record], new_records: List[str], delimiter: str) -> List[Record]:
+def update_records(
+    current_records: List[Record], new_records: List[str], delimiter: str
+) -> List[Record]:
+    """Combine existing records with records parsed from new records
+
+    Args:
+        current_records: Existing records
+        new_records: list str to parse into records
+        delimiter: separator to use when parsing new records
+    Returns:
+        A list containing both the new and existing records
+    Raises:
+        ValueError if one of the new records could not be parsed
+    """
     combined_records = current_records.copy()
     for line in new_records:
         fields = [field.strip() for field in line.split(delimiter)]
+
+        if len(fields) != 5:
+            raise ValueError(f"record '{line}' does not have 5 fields")
+
+        gender = Gender(fields[2])
+        date_of_birth = datetime.strptime(fields[4], "%m/%d/%Y").date()
+
         combined_records.append(
             Record(
                 last_name=fields[0],
                 first_name=fields[1],
-                gender=Gender(fields[2]),
+                gender=gender,
                 favorite_color=fields[3],
-                date_of_birth=datetime.strptime(fields[4], '%m/%d/%Y').date(),
+                date_of_birth=date_of_birth,
             )
         )
     return combined_records
 
 
 def records_sorted_by_gender_and_last_name(records: List[Record]) -> List[Record]:
-    return sorted(records, key=operator.attrgetter('gender', 'last_name'))
+    return sorted(records, key=operator.attrgetter("gender", "last_name"))
 
 
 def records_sorted_by_date_of_birth(records: List[Record]) -> List[Record]:
-    return sorted(records, key=operator.attrgetter('date_of_birth'))
+    return sorted(records, key=operator.attrgetter("date_of_birth"))
 
 
 def records_sorted_by_last_name_descending(records: List[Record]) -> List[Record]:
-    return sorted(records, key=operator.attrgetter('last_name'), reverse=True)
+    return sorted(records, key=operator.attrgetter("last_name"), reverse=True)
 
 
 def records_sorted_by_order(records: List[Record], order: RecordSortOrder):
